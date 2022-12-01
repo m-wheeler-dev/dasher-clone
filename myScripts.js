5ab028bd2d4f857016b1e11bbcb49beac521a181
Matthew <105622101+m-wheeler93@users.noreply.github.com>
2022-11-30 17:36:25 -0800

    Updated lets -> const and i++ -> i+=1

--- myScripts.js
+++ myScripts.js
@@ -1,26 +1,26 @@
 // RESPONSIVE NAV
-let navBtn = document.querySelector("#menu-icon");
-let navMenu = document.querySelector("nav");
+const navBtn = document.querySelector("#menu-icon");
+const navMenu = document.querySelector("nav");
 
 navBtn.addEventListener("click", () => {
 	navMenu.classList.toggle("responsive-nav-menu");
 });
 
 // NAV DROPDOWNS
-let navItems = document.getElementsByClassName("nav-menu-item");
+const navItems = document.getElementsByClassName("nav-menu-item");
 
-for (i = 0; i < navItems.length; i++) {
-	let submenu = navItems[i];
+for (i = 0; i < navItems.length; i += 1) {
+	const submenu = navItems[i];
 	submenu.addEventListener("click", () => {
 		submenu.classList.toggle("open-submenu");
 	});
 }
 
 // FAQ DROPDOWNS
-let faqItems = document.getElementsByClassName("faq-item");
+const faqItems = document.getElementsByClassName("faq-item");
 
-for (i = 0; i < faqItems.length; i++) {
-	let answer = faqItems[i];
+for (i = 0; i < faqItems.length; i += 1) {
+	const answer = faqItems[i];
 	answer.addEventListener("click", () => {
 		answer.classList.toggle("show-answer");
 	});
